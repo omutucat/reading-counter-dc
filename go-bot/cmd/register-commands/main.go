95f6038c4a99e5a5443b7cc783497632949adc6e
omutucat <y.kuma1102@gmail.com>
2025-08-30 23:44:23 +0900

    feat: add func register-commands

---
+++ go-bot/cmd/register-commands/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+	"reading-counter-dc/go-bot/internal/commands"
+)
+
+func main() {
+	// .envファイルなどから設定を読み込むことを想定
+	botToken := os.Getenv("DISCORD_BOT_TOKEN")
+	appID := os.Getenv("DISCORD_APP_ID")
+	guildID := os.Getenv("DISCORD_GUILD_ID") // 特定のサーバーのみに登録する場合
+
+	if botToken == "" || appID == "" {
+		log.Fatal("DISCORD_BOT_TOKEN and DISCORD_APP_ID must be set")
+	}
+
+	s, err := discordgo.New("Bot " + botToken)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+
+	fmt.Println("Registering commands...")
+	// internal/commands/commands.go で定義したコマンド情報をループして登録
+	registeredCommands, err := s.ApplicationCommandBulkOverwrite(appID, guildID, commands.All)
+	if err != nil {
+		log.Fatalf("Could not register commands: %v", err)
+	}
+
+	fmt.Printf("Successfully registered %d commands.\n", len(registeredCommands))
+}
---
+++ go-bot/internal/commands/commands.go
@@ -0,0 +1,11 @@
+package commands
+
+import "github.com/bwmarrin/discordgo"
+
+// All は、このBotで定義されるすべてのコマンドの定義を保持します。
+var All = []*discordgo.ApplicationCommand{
+	{
+		Name:        "ping",
+		Description: "Botの疎通確認をします",
+	},
+}
